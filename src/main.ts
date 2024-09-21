656534e81f3078df8e1baa296f3314f9d3599427
MJayConstantino <mjconstantino12345@gmail.com>
2024-09-22 00:51:33 +0800

    updated the html and css to kind of match the design of a real life scical model

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,1 @@
 import './style.css'
-
-function add(a: number, b: number): number {
-    return a + b;
-  }
-  
-function subtract(a: number, b: number): number {
-    return a - b;
-  }
-
-
-  function multiply(a: number, b: number): number {
-    return a * b;
-  }
-function divide(a: number, b: number): number {
-    return a / b;
-  }
